14e4ea6698d61080174c5894ef7f872940c58fc8
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-01 11:20:09 -0300

    🤞

--- packages/core/src/scroll.ts
+++ packages/core/src/scroll.ts
@@ -2,30 +2,29 @@
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
 const keySet = new Set([37, 38, 39, 40])
 
-function preventDefault(e: Event) {
-  e.preventDefault()
+function preventDefault(event: Event) {
+  event.preventDefault()
 }
 
-function preventDefaultForScrollKeys(e: KeyboardEvent) {
-  if (keySet.has[e.keyCode]) {
-    preventDefault(e)
+function preventDefaultForScrollKeys(event: KeyboardEvent) {
+  if (keySet.has[event.keyCode]) {
+    preventDefault(event)
     return false
   }
 }
 
 const wheelOpt = { passive: false }
-const wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel"
 
 export function disableScroll() {
   window.addEventListener("DOMMouseScroll", preventDefault)
-  window.addEventListener(wheelEvent, preventDefault, wheelOpt)
+  window.addEventListener("wheel", preventDefault, wheelOpt)
   window.addEventListener("touchmove", preventDefault, wheelOpt)
   window.addEventListener("keydown", preventDefaultForScrollKeys)
 }
 
 export function enableScroll() {
   window.removeEventListener("DOMMouseScroll", preventDefault)
-  window.removeEventListener(wheelEvent, preventDefault)
+  window.removeEventListener("wheel", preventDefault)
   window.removeEventListener("touchmove", preventDefault)
   window.removeEventListener("keydown", preventDefaultForScrollKeys)
 }
