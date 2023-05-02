59352ee27f2b26ebadb6e1812f62b8e066883664
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-02 19:06:57 -0300

    ❤️‍🔥

--- docs/components/Footer.vue
+++ docs/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
   <p :class="$style.footer">
-    Made with love by <a target="_blank" rel="noopener noreferrer" href="https://namnguyen.design">Nam Nguyen</a>
+    Made with love by
+    <a :class="$style.blog" target="_blank" rel="noopener noreferrer" href="https://namnguyen.design">Nam Nguyen</a>
   </p>
 </template>
 
@@ -8,5 +9,10 @@
 .footer {
   text-align: center;
   margin-top: 16px;
+}
+
+.blog {
+  color: violet;
+  font-weight: bold;
 }
 </style>
