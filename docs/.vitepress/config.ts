bf76666734df6db40f2363586368163b665bacad
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-03 18:15:32 -0300

    Add nav bar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,12 +1,24 @@
 import Unocss from "unocss/vite"
 import { defineConfig } from "vitepress"
+const pkg = require("../../packages/core/package.json")
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Zoom Image",
   description: "A little yet powerful library to zoom image on wheel / hover / pinch actions",
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-configp
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      {
+        text: pkg.version,
+        items: [
+          {
+            text: "Changelog",
+            link: "https://github.com/willnguyen1312/zoom-image/blob/main/packages/core/CHANGELOG.md",
+          },
+        ],
+      },
+    ],
 
     sidebar: [
       {
