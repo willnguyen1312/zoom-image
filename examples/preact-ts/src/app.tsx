b3d0383493ad2473bc1dc0a29eae61747e69401d
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-04 08:59:40 -0300

    docs: add react example

--- examples/preact-ts/src/app.tsx
+++ examples/preact-ts/src/app.tsx
@@ -58,16 +58,15 @@ function App() {
     <div class="font-sans">
       <nav class="flex space-x-4 pb-4" aria-label="Tabs">
         {tabs.map((tab) => {
-          const className =
-            "rounded-md px-3 py-2 text-sm font-medium decoration-none " +
-            (tab.current ? "bg-gray-100 text-gray-700" : "text-gray-500 hover:text-gray-700")
-
           return (
             <a
               onClick={makeHandleTabClick(tab)}
               key={tab.name}
               href={tab.href}
-              className={className}
+              class={
+                "rounded-md px-3 py-2 text-sm font-medium decoration-none " +
+                (tab.current ? "bg-gray-100 text-gray-700" : "text-gray-500 hover:text-gray-700")
+              }
               aria-current={tab.current ? "page" : undefined}
             >
               {tab.name}
--- examples/react-ts/vite.config.ts
+++ examples/react-ts/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+// eslint-disable-next-line import/no-unresolved
+import UnoCSS from "unocss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), UnoCSS()],
 })
