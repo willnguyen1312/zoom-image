457eb29030312dd7ddd6bdfb0ceac40febf07038
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-06 11:11:35 -0300

    chore: add clamp function tests

---
+++ packages/core/test/utils.test.ts
@@ -0,0 +1,35 @@
+import fc from "fast-check"
+import { faker } from "@faker-js/faker"
+import { clamp } from "../src/utils"
+
+describe("clamp functions", () => {
+  it("should work for value in between inclusively", () => {
+    fc.assert(
+      fc.property(fc.integer(), fc.integer(), (first, second) => {
+        const [min, max] = [Math.min(first, second), Math.max(first, second)]
+        const value = faker.datatype.number({ min, max })
+        expect(clamp(value, min, max)).toBe(value)
+      }),
+    )
+  })
+
+  it("should work for value under bound", () => {
+    fc.assert(
+      fc.property(fc.integer(), fc.integer(), (first, second) => {
+        const [min, max] = [Math.min(first, second), Math.max(first, second)]
+        const value = faker.datatype.number({ min: Number.MIN_SAFE_INTEGER, max: min - 1 })
+        expect(clamp(value, min, max)).toBe(min)
+      }),
+    )
+  })
+
+  it("should work for value upper bound", () => {
+    fc.assert(
+      fc.property(fc.integer(), fc.integer(), (first, second) => {
+        const [min, max] = [Math.min(first, second), Math.max(first, second)]
+        const value = faker.datatype.number({ min: max + 1, max: Number.MAX_SAFE_INTEGER })
+        expect(clamp(value, min, max)).toBe(max)
+      }),
+    )
+  })
+})
