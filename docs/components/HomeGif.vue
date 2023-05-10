e72271d96bb5f4b51a6f1d6b1ac08db668c2c601
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-10 10:24:12 -0300

    docs: update demo image sizing 🤞 (#42)

--- docs/components/HomeGif.vue
+++ docs/components/HomeGif.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex justify-center m-x-auto mt-4 px-6 sm:px-12">
-    <img alt="Demo Gif" class="h-auto max-w-full rounded-4" src="/demo.gif" />
+    <img alt="Demo Gif" class="h-auto max-w-full sm:h-[480px] sm:w-[480px] rounded-4" src="/demo.gif" />
   </div>
 </template>
