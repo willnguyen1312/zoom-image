015229077916799e08162d57264ec2b026a172cb
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-22 08:48:33 -0300

    Add Preact adapter 💞 (#126)
    * feat: add preact adapter 💞
    
    * chore: update doc 🧹

--- packages/docs/src/api/adapters/preact.md
+++ packages/docs/src/api/adapters/preact.md
@@ -1,1 +1,45 @@
-# Coming soon
+<script setup>
+import BundleSize from '../../components/BundleSize.vue'
+</script>
+
+# Zoom Image Preact
+
+The @zoom-image/preact adapter is a wrapper around the core zoom image logic.
+
+## `useZoomImageWheel` - <BundleSize func="useZoomImageWheel" pkg="@zoom-image/preact" />
+
+```ts
+function useZoomImageWheel(): {
+  createZoomImage
+  zoomImageState
+  setZoomImageState
+}
+```
+
+## `useZoomImageHover` - <BundleSize func="useZoomImageHover" pkg="@zoom-image/preact" />
+
+```ts
+function useZoomImageHover(): {
+  createZoomImage
+  zoomImageState
+  setZoomImageState
+}
+```
+
+## `useZoomImageMove` - <BundleSize func="useZoomImageMove" pkg="@zoom-image/preact" />
+
+```ts
+function useZoomImageMove(): {
+  createZoomImage
+  zoomImageState
+}
+```
+
+## `useZoomImageClick` - <BundleSize func="useZoomImageClick" pkg="@zoom-image/preact" />
+
+```ts
+function useZoomImageClick(): {
+  createZoomImage
+  zoomImageState
+}
+```
---
+++ packages/preact/src/useZoomImageHover.ts
@@ -0,0 +1,35 @@
+import { useCallback, useEffect, useRef, useState } from "preact/hooks"
+import { createZoomImageHover as _createZoomImageHover } from "@zoom-image/core"
+import { noop } from "./shared"
+
+import type { ZoomImageHoverState } from "@zoom-image/core"
+
+export function useZoomImageHover() {
+  const result = useRef<ReturnType<typeof _createZoomImageHover>>()
+  const [zoomImageState, updateZoomImageState] = useState<ZoomImageHoverState>({
+    enabled: false,
+    zoomedImgStatus: "idle",
+  })
+
+  const createZoomImage = useCallback((...arg: Parameters<typeof _createZoomImageHover>) => {
+    result.current?.cleanup()
+    result.current = _createZoomImageHover(arg[0], arg[1])
+    updateZoomImageState(result.current.getState())
+
+    result.current.subscribe((state) => {
+      updateZoomImageState(state)
+    })
+  }, [])
+
+  useEffect(() => {
+    return () => {
+      result.current?.cleanup()
+    }
+  }, [])
+
+  return {
+    createZoomImage,
+    zoomImageState,
+    setZoomImageState: result.current?.setState ?? noop,
+  }
+}
