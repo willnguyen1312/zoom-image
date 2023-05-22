b2367b6dbb1a01cc16315565e9290ab730578129
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-22 08:39:36 -0300

    feat: add preact adapter 💞

---
+++ packages/preact/src/useZoomImageWheel.ts
@@ -0,0 +1,37 @@
+import { useCallback, useEffect, useRef, useState } from "preact/hooks"
+import { createZoomImageWheel as _createZoomImageWheel } from "@zoom-image/core"
+import { noop } from "./shared"
+
+import type { ZoomImageWheelState } from "@zoom-image/core"
+
+export function useZoomImageWheel() {
+  const result = useRef<ReturnType<typeof _createZoomImageWheel>>()
+  const [zoomImageState, updateZoomImageState] = useState<ZoomImageWheelState>({
+    currentPositionX: -1,
+    currentPositionY: -1,
+    currentZoom: -1,
+    enable: false,
+  })
+
+  const createZoomImage = useCallback((...arg: Parameters<typeof _createZoomImageWheel>) => {
+    result.current?.cleanup()
+    result.current = _createZoomImageWheel(arg[0], arg[1])
+    updateZoomImageState(result.current.getState())
+
+    result.current.subscribe((state) => {
+      updateZoomImageState(state)
+    })
+  }, [])
+
+  useEffect(() => {
+    return () => {
+      result.current?.cleanup()
+    }
+  }, [])
+
+  return {
+    createZoomImage,
+    zoomImageState,
+    setZoomImageState: result.current?.setState ?? noop,
+  }
+}
