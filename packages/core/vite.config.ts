d6b4d94c27725d77be39dde83f4033f320d9ad50
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-28 08:36:02 -0300

    🎢

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -6,7 +6,7 @@ import Vue from "@vitejs/plugin-vue"
 export default defineConfig({
   plugins: [Vue()],
   test: {
-    setupFiles: ["./vite.setup.ts"],
+    setupFiles: ["./vitest.setup.ts"],
     globals: true,
     environment: "happy-dom",
   },
