528e040934bc56b0e02707206cd93d2f7748cd02
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-07-14 08:21:16 -0300

    Update footer link 💞

--- packages/docs/src/components/FooterComp.vue
+++ packages/docs/src/components/FooterComp.vue
@@ -5,7 +5,7 @@
       class="dark:text-violet font-bold text-[#7c447c]"
       target="_blank"
       rel="noopener noreferrer"
-      href="https://namnguyen.design"
+      href="https://namnguyen.design/about"
       >Nam Nguyen</a
     >
   </p>
