af410217757fbd87c37d41579bef9efebfff1c04
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-10-07 08:23:38 -0300

    🧹

--- packages/docs/src/components/HomePageShow.vue
+++ packages/docs/src/components/HomePageShow.vue
@@ -87,7 +87,6 @@ watch(
     if (zoomType.value === "move") {
       createZoomImageMove(imageMoveContainerRef.value as HTMLDivElement, {
         zoomImageSource: "https://nam-assets.netlify.app/static/large.webp",
-        disableScrollLock: true,
       })
     }
 
