5a3a63bc6d5f4fb2081dd8e63d7e01b5246ea9a6
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-12-04 09:26:33 -0400

    FIx core test 🔧

--- packages/core/test/components/TestImageZoomWheel.vue
+++ packages/core/test/components/TestImageZoomWheel.vue
@@ -43,7 +43,7 @@ watch(
       const zoomTarget = zoomTargetRef.value
 
       const result = createZoomImageHover(imageContainer, {
-        zoomImageSource: "/sample.avif",
+        zoomImageSource: "",
         customZoom: { width: 300, height: 500 },
         zoomTarget,
         scale: 2,
@@ -80,11 +80,11 @@ onUnmounted(() => {
     </nav>
 
     <div v-if="zoomType === 'wheel'" ref="imageWheelContainerRef">
-      <img alt="Large Pic" src="/sample.avif" />
+      <img alt="Large Pic" src="" />
     </div>
 
     <div v-if="zoomType === 'hover'" ref="imageHoverContainerRef">
-      <img alt="Small Pic" src="/sample.avif" />
+      <img alt="Small Pic" src="" />
       <div ref="zoomTargetRef" data-testid="zoomTarget"></div>
     </div>
   </div>
