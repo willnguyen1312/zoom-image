e4f16e3164144d8de2010792a9d84a86a30d3579
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-12-27 13:33:54 -0400

    Upgrade all dependencies 💞

--- packages/docs/src/components/DemoComp.vue
+++ packages/docs/src/components/DemoComp.vue
@@ -6,7 +6,7 @@ defineProps<{ url: string }>()
   <div class="min-h-3xl mt-6 flex w-full flex-col">
     <iframe
       :src="url"
-      class="rounded-4 flex-1 overflow-hidden border-transparent bg-white shadow-xl shadow-gray-700/20 dark:bg-black lg:rounded-l-2xl"
+      class="rounded-4 flex-1 overflow-hidden border-transparent bg-white shadow-xl shadow-gray-700/20 lg:rounded-l-2xl dark:bg-black"
     />
   </div>
 </template>
